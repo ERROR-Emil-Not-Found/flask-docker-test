a0914ee5e7cceb94a3625fd903ea391b061c2106
ERROR Emil Not Found <fyoug8gle@gmail.com>
2022-12-29 17:10:54 +0100

    .

--- app/app/core/app_setup.py
+++ app/app/core/app_setup.py
@@ -1,10 +1,16 @@
 from ..api import api  # noqa
 from ..main import app
+from os import getcwd
 
 
 @app.route("/")
 def hello():
     # This could also be returning an index.html
-    return """Hello World from Flask in a uWSGI Nginx Docker container with \
+
+    with open("temp.txt", 'a+') as f:
+        f.write("hello world\n")
+    return f"""Hello World from Flask in a uWSGI Nginx Docker container with \
      Python 3.7 (from the example template), 
-     try also: <a href="/users/">/users/</a>"""
+     try also: <a href="/users/">/users/</a>
+     
+     current working directory: {getcwd()}"""
